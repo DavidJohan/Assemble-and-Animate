34dc3d544b0c9074c7533f1156e3538cdb0b5ada
David Johan Christensen <djchr@elektro.dtu.dk>
2011-06-22 15:33:04 +0200

    added to header for easy use of framework layer

---
+++ libASE/include/ase/framework.h
@@ -0,0 +1,4 @@
+#include "./infrastructure/Scheduler/Scheduler.h"
+#include "./infrastructure/EventManager/EventManager.h"
+#include "./infrastructure/MsgManager/MsgManager.h"
+#include "./infrastructure/MemManager/MemManager.h"
